57083c2addad4417114fe0069a65e10ca56fd884
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-31 15:00:17 -0500

    Init

---
+++ applications/app-service/src/main/java/co/com/plantinfo/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.plantinfo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.plantinfo.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
