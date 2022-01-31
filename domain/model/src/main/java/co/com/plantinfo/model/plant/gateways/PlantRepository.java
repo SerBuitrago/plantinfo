6d41fcfc3abc9effcf89bb446e569ff899d6dfb9
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-31 16:20:08 -0500

    Plant user case

--- domain/model/src/main/java/co/com/plantinfo/model/plant/gateways/PlantRepository.java
+++ domain/model/src/main/java/co/com/plantinfo/model/plant/gateways/PlantRepository.java
@@ -1,4 +1,20 @@
 package co.com.plantinfo.model.plant.gateways;
 
+import co.com.plantinfo.model.TypePlant;
+import co.com.plantinfo.model.plant.Plant;
+
+import java.util.List;
+
 public interface PlantRepository {
+    Plant findById(Long id);
+
+    Plant findByName(String name);
+
+    List<Plant> findByTypeAll(TypePlant type);
+
+    Plant save(Plant plant);
+
+    Plant update(Plant plant);
+
+    Plant deleteById(Long id);
 }
---
+++ domain/usecase/src/main/java/co/com/plantinfo/usecase/plant/PlantUseCase.java
@@ -0,0 +1,38 @@
+package co.com.plantinfo.usecase.plant;
+
+import co.com.plantinfo.model.TypePlant;
+import co.com.plantinfo.model.plant.Plant;
+import co.com.plantinfo.model.plant.gateways.PlantRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class PlantUseCase{
+
+    private final PlantRepository plantRepositoryGateway;
+
+    public Plant findById(Long id) {
+        return plantRepositoryGateway.findById(id);
+    }
+
+    public Plant findByName(String name) {
+        return plantRepositoryGateway.findByName(name);
+    }
+
+    public List<Plant> findByTypeAll(TypePlant type) {
+        return plantRepositoryGateway.findByTypeAll(type);
+    }
+
+    public Plant save(Plant plant) {
+        return plantRepositoryGateway.save(plant);
+    }
+
+    public Plant update(Plant plant) {
+        return plantRepositoryGateway.update(plant);
+    }
+
+    public Plant deleteById(Long id) {
+        return plantRepositoryGateway.deleteById(id);
+    }
+}
